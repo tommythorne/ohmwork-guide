fe37ae9781fdfab7de4b58fa7e9cd8efe0d289c3
tommythorne <64710017+tommythorne@users.noreply.github.com>
2025-08-14 23:52:34 +0000

    Add both Unsplash image domains to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
